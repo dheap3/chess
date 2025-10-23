7cb75a37d6efa7b1a1c4f065aa9339371266c74d
dheap3 <davidjamesheap@gmail.com>
2025-10-23 00:29:31 -0600

    stores data correctly in memory now (still has comments of old code)

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,22 @@
 package server;
 
 import com.google.gson.Gson;
+import dataModel.AuthData;
+import dataModel.UserData;
 import io.javalin.*;
 import io.javalin.http.Context;
 import service.userService;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Server {
 
     private final Javalin server;
     private userService userService = new userService();
+    private Map<String, UserData> users = new HashMap<String, UserData>();
+    private Map<String, AuthData> auths = new HashMap<String, AuthData>();
+
 
     public Server() {
         server = Javalin.create(config -> config.staticFiles.add("web"));
@@ -24,6 +30,11 @@ public class Server {
         var serializer = new Gson();
         var req = serializer.fromJson(ctx.body(), Map.class);
         userService.register(req);
+
+        UserData user = userService.getUser((String) req.get("username"), users);
+        users.put(user.getUsername(), user);
+        AuthData auth = userService.CreateAuth(user.getUsername(), user.getPassword(), user.getEmail());
+        auths.put(auth.getUsername(), auth);
 
 
     }
--- server/src/main/java/service/userService.java
+++ server/src/main/java/service/userService.java
@@ -7,25 +7,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class userService {
-    private Map<String, UserData> users = new HashMap<String, UserData>();
-    private Map<String, AuthData> auths = new HashMap<String, AuthData>();
+//    private Map<String, UserData> users = new HashMap<String, UserData>();
+//    private Map<String, AuthData> auths = new HashMap<String, AuthData>();
 
-
-    public AuthData register(Map<String, Object> req) {
+    public void register(Map<String, Object> req) {
         String username = (String) req.get("username");
         String password = (String) req.get("password");
         String email = (String) req.get("email");
 
-        if (getUser(username) != null) {
-            //fix exception here
-//            Exception AlreadyTakenException = null;
-//            throw AlreadyTakenException;
-        }
+//        if (getUser(username) != null) {
+//            //fix exception here
+////            Exception AlreadyTakenException = null;
+////            throw AlreadyTakenException;
+//        }
         createUser(username, password, email);
         CreateAuth(username, password, email);
-        return getAuth(username);
+//        return getAuth(username);
     }
-    public UserData getUser(String username) {
+    public UserData getUser(String username, Map<String, UserData> users) {
         //get the user from the db
         if (users.containsKey(username)) {
             var user = users.get(username);
@@ -34,18 +33,19 @@ public class userService {
             return null;
         }
     }
-    public boolean createUser(String username, String password, String email) {
+    public UserData createUser(String username, String password, String email) {
         //create user
         var user = new UserData(username, password, email);
         //add to db
-        users.put(username, user);
-        if (users.containsKey(username)) {
-            return true;
-        } else {
-            return false;
-        }
+        return user;
+//        users.put(username, user);
+//        if (users.containsKey(username)) {
+//            return true;
+//        } else {
+//            return false;
+//        }
     }
-    public AuthData getAuth(String username) {
+    public AuthData getAuth(String username, Map<String, AuthData> auths) {
         //get the auth from the db
         if (auths.containsKey(username)) {
             var auth = auths.get(username);
@@ -54,17 +54,18 @@ public class userService {
             return null;
         }
     }
-    public boolean CreateAuth(String username, String password, String email) {
+    public AuthData CreateAuth(String username, String password, String email) {
         //create authToken
         var authToken = password + ":" + email;
         AuthData auth = new AuthData(username, authToken);
         //add to db
-        auths.put(username, auth);
-        if (auths.containsKey(username)) {
-            return true;
-        } else {
-            return false;
-        }
+        return auth;
+//        auths.put(username, auth);
+//        if (auths.containsKey(username)) {
+//            return true;
+//        } else {
+//            return false;
+//        }
     }
 
 }

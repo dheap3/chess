fc92c10d2f3e3a875c8f0dd8ef79fa7b89cd2799
dheap3 <davidjamesheap@gmail.com>
2025-11-12 10:50:59 -0700

    postlogin skeleton

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -8,10 +8,13 @@ public class Main {
         boolean exit = false;
         String input;
         System.out.println("♕ Welcome to CS 240 Chess! ♕\nEnter one of the following options:");
+
+        //prelogin ui
         printOptions();
         System.out.print("[LOGGED OUT] ->> ");
         while (!exit) {
             input = scanner.nextLine();
+            input = input.toLowerCase();
             switch (input) {
                 case "help":
                     printOptions();
@@ -24,6 +27,7 @@ public class Main {
                 case "login":
                     //Prompts the user to input login information. Calls the server login API to login the user. When successfully logged in, the client should transition to the Postlogin UI.
                     System.out.println("login here please");
+                    postLoginUI();
                     break;
                 case "register":
                     //Prompts the user to input registration information. Calls the server register API to register and login the user. If successfully registered, the client should be logged in and transition to the Postlogin UI.
@@ -37,6 +41,34 @@ public class Main {
         }
 
     }
+
+    static void postLoginUI() {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        String input;
+        System.out.print("[LOGGED OUT] ->> ");
+        while (!exit) {
+            input = scanner.nextLine();
+            input = input.toLowerCase();
+            //should this be one word? how should i read the other input?
+            switch (input) {
+                case "help":
+                    break;
+                case "logout":
+                    break;
+                case "create game":
+                    break;
+                case "list games":
+                    break;
+                case "play game":
+                    break;
+                case "observe game":
+                    break;
+
+            }
+        }
+    }
+
     static void printOptions() {
         System.out.print("help - display this help menu\n" +
                 "login <username> <password> - to log in\n" +

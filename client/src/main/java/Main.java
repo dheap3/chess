02a1053a9dc0b2897c5b4f489f1624f0c270f8d0
dheap3 <davidjamesheap@gmail.com>
2025-11-15 09:26:34 -0700

    took out extra long lines of comments in main describing methods. refer to the github

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -36,7 +36,6 @@ public class Main {
                     } else {
                         System.out.println(args[1] + " = username");
                         System.out.println(args[2] + " = password");
-                        //Prompts the user to input login information. Calls the server login API to log in the user. When successfully logged in, the client should transition to the Postlogin UI.
                         postLoginUI();
                         break;
                     }
@@ -48,7 +47,6 @@ public class Main {
                         System.out.println(args[1] + " = username");
                         System.out.println(args[2] + " = password");
                         System.out.println(args[3] + " = email");
-                        //Prompts the user to input registration information. Calls the server register API to register and login the user. If successfully registered, the client should be logged in and transition to the Postlogin UI.
                         postLoginUI();
                         break;
                     }
@@ -81,7 +79,6 @@ public class Main {
                     System.out.print("[LOGGED IN] ->> ");
                     break;
                 case "logout":
-                    //Logs out the user. Calls the server logout API to log out the user. After logging out with the server, the client should transition to the Prelogin UI.
                     exit = true;
                     break;
                 case "create":
@@ -91,11 +88,9 @@ public class Main {
                     } else {
                         gameName = args[1];
                         System.out.println(gameName + " = gamename");
-                        //Allows the user to input a name for the new game. Calls the server create API to create the game. This does not join the player to the created game; it only creates the new game in the server.
                         break;
                     }
                 case "list":
-                    //Lists all the games that currently exist on the server. Calls the server list API to get all the game data, and displays the games in a numbered list, including the game name and players (not observers) in the game. The numbering for the list should be independent of the game IDs and should start at 1.
                     System.out.println("heres all the games you need:");
                     break;
                 case "join":
@@ -112,7 +107,6 @@ public class Main {
                             System.out.println("Join failed. Please enter a valid game ID/COLOR (COLOR must be all caps)");
                             break;
                         }
-                        //Allows the user to specify which game they want to join and what color they want to play. They should be able to enter the number of the desired game. Your client will need to keep track of which number corresponds to which game from the last time it listed the games. Calls the server join API to join the user to the game.
                         System.out.println(gameID + " = gameID");
                         System.out.println(color + " = color");
                         gameUI(gameID, color);
@@ -124,7 +118,6 @@ public class Main {
                         break;
                     } else {
                         gameID = Integer.parseInt(args[1]);
-                        //Allows the user to specify which game they want to observe. They should be able to enter the number of the desired game. Your client will need to keep track of which number corresponds to which game from the last time it listed the games. Additional functionality will be added in Phase 6.
                         System.out.println(gameID + " = game ID");
                         gameUI(gameID, color);
                         break;

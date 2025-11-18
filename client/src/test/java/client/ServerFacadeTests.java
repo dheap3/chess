857bbcecec8b5bc225a66a99b07c90a48e665f76
dheap3 <davidjamesheap@gmail.com>
2025-11-17 17:12:41 -0700

    register tests added

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,25 @@
 package client;
 
+import datamodel.ErrorResponse;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -20,10 +27,28 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    public void before() {
+        facade.clear();
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerGood() {
+        try {
+            var authData = facade.register("player1", "password", "p1@email.com");
+            assertTrue(authData.authToken().length() > 10);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+    @Test
+    void registerBad() {
+        try {
+            var authData = facade.register("player2", "password", "p2@email.com");
+            assertNotEquals("player1", authData.username());
+        }  catch (Exception e) {
+            fail();
+        }
     }
 
 }

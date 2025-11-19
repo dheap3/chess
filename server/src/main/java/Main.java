56f42bf4697128e7c394e7ec9bc1622e5b10221f
dheap3 <davidjamesheap@gmail.com>
2025-11-19 10:50:16 -0700

    server prints pretty now

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -7,9 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: ");
         server = new Server();
         var port = server.run(0);
-        System.out.println("Started test HTTP server on " + port);
+        System.out.println("♕ 240 Chess Server: " + port);
     }
 }

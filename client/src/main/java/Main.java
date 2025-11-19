2e35db25395d56d5b707de98c7b9979dbc4a71d9
dheap3 <davidjamesheap@gmail.com>
2025-11-19 15:27:06 -0700

    observe no longer breaks on invalid games added custom error messages when something goes wrong

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("♕ Welcome to CS 240 Chess! ♕\nEnter one of the following options:");
-        int port = 54350;
+        int port = 56748;
         String url = "http://localhost:" + port;
         facade = new ServerFacade(port);
         preloginUI();
@@ -37,25 +37,21 @@ public class Main {
                     break;
                 case "quit", "exit":
                     exit = true;
-                    //stop server here?
                     break;
                 case "login":
                     try {
                         facade.login(args[1], args[2]);
                     } catch (Exception e) {
-                        System.out.println("Login failed. Please enter a valid USERNAME/PASSWORD");
-//                            System.out.println(e.toString());
+                        printError(e, "login");
                         break;
                     }
-//                        facadeResponseHandler(args, "login");
                     postLoginUI();
                     break;
                 case "register":
                     try {
                         facade.register(args[1], args[2], args[3]);
                     } catch (Exception e) {
-                        System.out.println("Register failed, please enter a valid USERNAME/PASSWORD/EMAIL");
-//                        System.out.println(e.toString());
+                        printError(e, "register");
                         break;
                     }
                     postLoginUI();
@@ -110,8 +106,7 @@ public class Main {
                         var game = facade.createGame(gameName);
                         System.out.println("Created game " + gameName);
                     } catch (Exception e) {
-                        System.out.println("Create failed. Please enter a valid GAMENAME");
-//                            System.out.println(e.toString());
+                        printError(e, "create");
                         break;
                     }
                     break;
@@ -119,7 +114,7 @@ public class Main {
                     try {
                         var list = facade.listGames().getGames();
                         numGameID = new HashMap<Integer, Integer>();//reset our num to gameID map
-                        System.out.println("# : Game ID : Game Name : Users In Game WHITE|BLACK");
+                        System.out.println("# : Game Name : Users In Game WHITE|BLACK");
                         for (int i = 0; i < list.size(); i++) {
                             var game = list.get(i);
                             int num = i + 1;
@@ -129,10 +124,8 @@ public class Main {
                             String blackUser = (game.blackUsername() != null) ? game.blackUsername() : "(No User)";
                             System.out.print(whiteUser + " | " + blackUser + "\n");
                         }
-//                        System.out.println(list.toString());
                     } catch (Exception e) {
-                        System.out.println("List failed. Talk to the administrator :(");
-                        System.out.println(e.toString());
+                        printError(e, "list");
                         break;
                     }
                     break;
@@ -149,31 +142,28 @@ public class Main {
                             break;
                         }
                         facade.joinGame(color, gameID);
+                        gameUI(gameID, color);
                     } catch (Exception e) {
-                        System.out.println("Join failed. Please enter a valid game ID/COLOR");
-//                            System.out.println(e.toString());
+                        printError(e, "join");
                         break;
                     }
                     break;
                 case "observe":
-                    if (args.length != 2) {
-                        System.out.println("Observe failed. Please enter a valid game ID");
-                        break;
-                    } else {
+                    try {
                         int num = Integer.parseInt(args[1]);
                         int gameID = numGameID.get(num);
-//                        System.out.println(gameID + " = game ID");
-                        //default color to observe is white
-                        color = ChessGame.TeamColor.WHITE;
+                        color = ChessGame.TeamColor.WHITE;//default color to observe is white
                         gameUI(gameID, color);
                         break;
+                    } catch (Exception e) {
+                        printError(e, "observe");
                     }
                 default:
                     System.out.println("option not valid. please try again");
 
             }
         }
-    }
+    }//81
 
     static void gameUI(int gameID, ChessGame.TeamColor color) {
         Scanner scanner = new Scanner(System.in);
@@ -214,7 +204,92 @@ public class Main {
                 "logout - to log out\n" +
                 "create <GAMENAME> - create a game with the name GAMENAME\n" +
                 "list - list all games\n" +
-                "join <ID> [WHITE|BLACK] - join the game with game id ID as color WHITE|BLACK\n" +
-                "observe <ID> - observe the game with the game id ID\n");
+                "join <GAMENUM> [WHITE|BLACK] - join the game with game number GAMENUM as color WHITE|BLACK\n" +
+                "observe <GAMENUM> - observe the game with the game number GAMENUM\n");
+    }
+
+    static void printError(Exception e, String method) {
+        switch (method) {
+            case "create":
+                if (e.getMessage().startsWith("400")) {
+                    System.out.println("Create failed. Invalid GAMENAME");
+                }
+                if (e.getMessage().startsWith("401")) {
+                    System.out.println("Create failed. You are not authorized for that");
+                }
+                break;
+            case "list":
+//                System.out.println("List failed. Talk to the administrator :(");
+                if (e.getMessage().startsWith("401")) {
+                    System.out.println("List failed. You are not authorized for that");
+                }
+                break;
+            case "join":
+//                System.out.println("Join failed. Please enter a valid GAMENUM/COLOR");
+                if (e instanceof java.lang.IndexOutOfBoundsException ) {
+                    if (e.getMessage().endsWith("1")) {//length of the args should be 3
+                        System.out.println("Join failed. Please enter a valid GAMENUM/COLOR");
+                    } else if (e.getMessage().endsWith("2")) {
+                        System.out.println("Join failed. Please enter a valid COLOR");
+                    }
+                }
+                if (e.getMessage().startsWith("400")) {
+                    System.out.println("Join failed. Invalid GAMENUM");
+                }
+                if (e.getMessage().startsWith("401")) {
+                    System.out.println("Join failed. You are not authorized for that");
+                }
+                if (e.getMessage().startsWith("403")) {
+                    System.out.println("Join failed. COLOR already taken");
+                }
+                if (e instanceof java.lang.NullPointerException ) {
+                    System.out.println("Join failed. Game does not exist");
+                }
+                break;
+            case "observe":
+                if (e instanceof java.lang.IndexOutOfBoundsException ) {
+                    if (e.getMessage().endsWith("1")) {//length of the args should be 2
+                        System.out.println("Observe failed. Please enter a valid GAMENUM");
+                    }
+                }
+                if (e instanceof java.lang.NullPointerException ) {
+                    System.out.println("Observe failed. Game does not exist");
+                }
+                break;
+            case "login":
+                if (e instanceof java.lang.IndexOutOfBoundsException ) {
+                    if (e.getMessage().endsWith("1")) {//length of the args should be 3
+                        System.out.println("Login failed. Please enter a valid USERNAME/PASSWORD");
+                    } else if (e.getMessage().endsWith("2")) {
+                        System.out.println("Login failed. Please enter a valid PASSWORD");
+                    }
+                }
+                if (e instanceof java.lang.RuntimeException ) {
+                    if (e.getMessage().startsWith("401")) {
+                        System.out.println("Login failed. Invalid credentials");
+                    }
+                }
+                break;
+            case "register":
+                if (e instanceof java.lang.IndexOutOfBoundsException ) {
+                    if (e.getMessage().endsWith("1")) {//length of the args should be 4
+                        System.out.println("Register failed, please enter a valid USERNAME/PASSWORD/EMAIL");
+                    } else if (e.getMessage().endsWith("2")) {
+                        System.out.println("Register failed, please enter a valid PASSWORD/EMAIL");
+                    } else if (e.getMessage().endsWith("3")) {
+                        System.out.println("Register failed, please enter a valid EMAIL");
+                    }
+                }
+                if (e instanceof java.lang.RuntimeException ) {
+                    if (e.getMessage().startsWith("403")) {
+                        System.out.println("Register failed. Username already taken");
+                    }
+                }
+                break;
+            default:
+                System.out.println("option not valid. please try again");
+                break;
+        }
+//        System.out.println("error: " + e.getMessage());
     }
 }

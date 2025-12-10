41ed140eee51271e5656708dc2461105a78b1ce8
dheap3 <davidjamesheap@gmail.com>
2025-12-10 02:50:20 -0700

    extra code code in WebSocketFacade

--- client/src/main/java/ui/WebSocketFacade.java
+++ client/src/main/java/ui/WebSocketFacade.java
@@ -1,15 +1,10 @@
 package ui;
 
 import com.google.gson.Gson;
-//import org.glassfish.tyrus.core.wsadl.model.Endpoint;
-import ui.ServerObserver;
 import jakarta.websocket.*;
 import websocket.commands.UserGameCommand;
-import websocket.messages.ServerMessage;
 
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.WebSocket;
 
 public class WebSocketFacade extends Endpoint {
 
@@ -32,38 +27,8 @@ public class WebSocketFacade extends Endpoint {
 
     public void send(UserGameCommand cmd) throws Exception{
         String json = gson.toJson(cmd);
-//        System.out.println("Sending WS message: " + json);
-//        socket.sendText(json, true);
         session.getBasicRemote().sendText(json);
     }
-
-
-//    @Override
-//    public void onOpen(WebSocket webSocket) {
-//        System.out.println("Connected to server WS");
-//        webSocket.request(1);
-//    }
-
-//    @Override
-//    public java.util.concurrent.CompletionStage<?> onText(WebSocket ws, CharSequence data, boolean last) {
-//        System.out.println("Received WS message: " + data);
-//        String dataString = data.toString();
-//        ServerMessage msg = gson.fromJson(dataString, ServerMessage.class);
-//        switch (msg.getServerMessageType()) {
-//            case LOAD_GAME -> {
-//                //redraw the chessboard
-//                //boardText.printBoard()
-//            }
-//            case ERROR -> {
-//
-//            }
-//            case NOTIFICATION -> {
-//
-//            }
-//        }
-//        ws.request(1);
-//        return null;
-//    }
 
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
